60b72368a6b7abb8b1084df705fb99ec9c615dd9
Oetzi <109589892+Bloetzi@users.noreply.github.com>
2022-07-19 20:41:19 +0200

    Create Time.lua

---
+++ Settings/Time.lua
@@ -0,0 +1,16 @@
+local m = require(script.Parent.Manager)
+local RS = game:GetService("RunService")
+
+if RS:IsRunning() then
+	return
+else
+	while true  do  --30 minutes  
+		task.wait(30*60)
+		if RS:IsRunning() then
+			break
+		end
+		if script.Parent.Values.Reminder.Value == true then
+			m:CreateTimer(script.Parent.TimeFrame)
+		end
+	end
+end
